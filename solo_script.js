278affe73f716937c189f31f4c9eff05479d2ad5
dougr1010 <dougritzinger@DougRitsMacBook.PK5001Z>
2015-12-06 11:36:25 -0600

    code clean up

--- solo_script.js
+++ solo_script.js
@@ -30,34 +30,31 @@ var array = [objectAtticus, objectJem, objectBoo, objectScout];
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
-
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
+
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
     //console.log("first loop: " + array[i]);
   	array[i] = calculateSTI(array[i]);          // Bug 1: need to add index so only sending in a 1D array
-    //console.log("first loop: " + array[i]);
+    array[i] = array[i].join(", ");  //add some space between array elements
+    //insert into DOM
    	newEl = document.createElement('li');
-    //add some space between array elements
-    array[i] = array[i].join(", ")
-    console.log(array[i]);
   	newText = document.createTextNode(array[i]);
   	newEl.appendChild(newText);
   	position.appendChild(newEl);
 }
 
+
+//calculate employee bonus
 function calculateSTI(personObject){
     var newArray = [];
-
     newArray[0] = personObject.name;
-
     var employeeNumber = personObject.number;
     var baseSalary = personObject.salary;
     var reviewScore = parseInt(personObject.rating);
-  //console.log("top of CalcSTI: " + newArray[0] + " " + employeeNumber + " " + baseSalary + " " + reviewScore);
 
     var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
     if(bonus > 0.13){
@@ -67,11 +64,12 @@ function calculateSTI(personObject){
     newArray[1] = bonus;
     newArray[2] = Math.round(baseSalary * (1.0 + bonus));       //bug 2: this needs to be an integer 
     newArray[3] = Math.round(baseSalary * bonus);
-    //console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
 
     return newArray;
 }
 
+
+//determine base bonus percent based on review score
 function getBaseSTI(reviewScore){
     var basePercent;
     switch(reviewScore){
@@ -94,6 +92,8 @@ function getBaseSTI(reviewScore){
     return basePercent; //basePercent - 1;    Bug 3: don't subtract 1
 }
 
+
+//adjustment for more senior employees
 function getYearAdjustment(employeeNumber){
     var yearAdjustment = 0;
     if(employeeNumber.length == 4){
@@ -102,6 +102,8 @@ function getYearAdjustment(employeeNumber){
     return yearAdjustment;
 }
 
+
+//adjustment for high income employees
 function getIncomeAdjustment(salary){
     var incomeAdjustment = 0;
     salary = parseInt(salary);
